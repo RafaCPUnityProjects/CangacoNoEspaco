97f983e665c041159d89aa239340d10a74839360
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-07-29 16:54:11 -0300

    Many updates

--- Cangaço no Espaço/Assets/Scripts/NordevJam/BossEnemyInstantiator.cs
+++ Cangaço no Espaço/Assets/Scripts/NordevJam/BossEnemyInstantiator.cs
@@ -9,15 +9,8 @@ public class BossEnemyInstantiator : MonoBehaviour
 	{
 		GameObject enemy = (GameObject)Instantiate(enemyPrefab, transform.position, Quaternion.identity);
 		NPCController npcController = enemy.GetComponent<NPCController>();
-		int body = PlayerPrefs.GetInt("BossHead", 0);
-		int head = PlayerPrefs.GetInt("BossHead", -1);
-		int face = PlayerPrefs.GetInt("BossHead", -1);
-		int shirt = PlayerPrefs.GetInt("BossHead", -1);
-		int pant = PlayerPrefs.GetInt("BossHead", 0);
-		int feet = PlayerPrefs.GetInt("BossHead", -1);
-		int knife = PlayerPrefs.GetInt("BossHead", 0);
-		BodyInfo bossBodyInfo = new BodyInfo(body, head, face, shirt, pant, feet, knife);
-		npcController.DressPlayer(bossBodyInfo);
+		
+		npcController.LoadBossInfo();
 		Destroy(this.gameObject);
 	}
 }
---
+++ Cangaço no Espaço/Assets/Scripts/NordevJam/PickableItem.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickableItem : MonoBehaviour
+{
+	public PickableItemType myType;
+	public float modifier = 1f;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.transform.parent.tag == "Player")
+		{
+			NPCController npcController = other.transform.parent.GetComponent<NPCController>();
+			ChangePlayer(npcController);
+			Destroy(this.gameObject);
+		}
+	}
+
+	void ChangePlayer(NPCController controller)
+	{
+		switch (myType)
+		{
+			case PickableItemType.normalWeapon:
+				controller.PickNormalWeapon(modifier);
+				break;
+			case PickableItemType.bigWeapon:
+				controller.PickBigWeapon(modifier);
+				break;
+			case PickableItemType.health:
+				controller.PickHealth(modifier);
+				break;
+			case PickableItemType.healthBuff:
+				controller.PickHealthBuff(modifier);
+				break;
+			case PickableItemType.speedBuff:
+				controller.PickSpeedBuff(modifier);
+				break;
+			case PickableItemType.strengthBuff:
+				controller.PickStrengthBuff(modifier);
+				break;
+			default:
+				break;
+		}
+	}
+}
+
+public enum PickableItemType
+{
+	normalWeapon,
+	bigWeapon,
+	health,
+	healthBuff,
+	speedBuff,
+	strengthBuff,
+}
