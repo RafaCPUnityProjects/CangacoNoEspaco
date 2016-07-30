78ba9c36a9d30270b94bda2241332ce27b2c7c94
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2016-07-30 18:41:30 -0300

    StartScene -> type stuff

---
+++ Cangaço no Espaço/Assets/Scripts/NordevJam/SlowTyper.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SlowTyper : MonoBehaviour
+{
+	public string fullText;
+	public float letterSpeed = 0.1f;
+	public Text typedText;
+	private string currentText;
+
+	void Start()
+	{
+		StartCoroutine(Type());
+	}
+
+	IEnumerator Type()
+	{
+		currentText = "";
+		for (int i = 0; i < fullText.Length; i++)
+		{
+			//currentText += fullText[i];
+			typedText.text += fullText[i];
+			yield return new WaitForSeconds(letterSpeed);
+		}
+	}
+}
